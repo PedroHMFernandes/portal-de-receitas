6b095139142d14784efc774338fd781f089e674f
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-10-22 17:43:37 -0300

    Project created

---
+++ portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/PortalDeReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.mackenzie.receitas.portal_receitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalDeReceitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalDeReceitasApplication.class, args);
+    }
+
+}
---
+++ portal-receitas/src/test/java/com/mackenzie/receitas/portal_receitas/PortalDeReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackenzie.receitas.portal_receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalDeReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
